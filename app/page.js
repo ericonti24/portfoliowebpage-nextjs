6fd45316031129fecab9ebd684014eed06f719a9
Eric <ericontiveros123@gmail.com>
2023-08-17 10:15:20 -0700

    updated todo list project pic

--- app/page.js
+++ app/page.js
@@ -1,13 +1,75 @@
+'use client'
 
-import React from 'react'
+import {
+  Box,
+  Center,
+  useColorModeValue,
+  Heading,
+  Text,
+  Stack,
+  Image,
+  Flex,
+  Spacer,
+  
+} from '@chakra-ui/react'
 
 
-const HomePage = () => {
+const IMAGE =
+  'https://scontent-lax3-2.xx.fbcdn.net/v/t1.15752-9/367683708_820190056268453_6084955692198487598_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=ae9488&_nc_ohc=rHVD4PHgUgYAX89ct6C&_nc_ht=scontent-lax3-2.xx&oh=03_AdTuZNcm5vyki17lHvCsBXwPIcOc_52WUf1r8ogqHPohsQ&oe=6504E0D5'
+
+export default function ProductSimple() {
   return (
-        <div>
-        <h1 style={{color: 'white'}}>Home Page!</h1>
-      </div>
+    <Flex>
+        <Box p='40'>
+        <Heading color={'white'} fontFamily='serif' size='4xl'>Hi, I'm Eric.</Heading>
+        <Text color='white' p='4'>Web designer, developer and tech enthusiast!</Text>
+        </Box>
+        <Spacer />
+          <Box
+            role={'group'}
+            // p={6}
+            p='4'
+            maxW={'330px'}
+            w={'full'}
+            bg={useColorModeValue('black')}
+            boxShadow={'2xl'}
+            rounded={'lg'}
+            // pos={'relative'}
+            zIndex={1}
+          >
+            <Box
+              rounded={'lg'}
+              // mt={-12}
+              pos={'relative'}
+              height={'230px'}
+              _after={{
+                transition: 'all .3s ease',
+                content: '""',
+                w: 'full',
+                h: 'full',
+                pos: 'absolute',
+                top: 5,
+                left: 0,
+                backgroundImage: `url(${IMAGE})`,
+                filter: 'blur(15px)',
+                zIndex: -1,
+              }}
+              _groupHover={{
+                _after: {
+                  filter: 'blur(20px)',
+                },
+              }}>
+              <Image
+                rounded={'lg'}
+                height={320}
+                width={282}
+                objectFit={'cover'}
+                src={IMAGE}
+                alt="#"
+              />
+            </Box>
+        </Box>
+    </Flex>
+    
   )
 }
-
-export default HomePage

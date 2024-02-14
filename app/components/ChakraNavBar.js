2dcda9fce6801704c636f0d126551cccc901292d
Eric <ericontiveros123@gmail.com>
2024-02-14 14:20:42 -0700

    re-style home page

--- app/components/ChakraNavBar.js
+++ app/components/ChakraNavBar.js
@@ -1,38 +1,56 @@
 'use client'
 
-import { Box, Button, Link, Container, Stack, HStack, Text, useColorModeValue, Flex } from '@chakra-ui/react'
+import { 
+  Box,  
+  Link, 
+  Flex, 
+  Heading, 
+  useMediaQuery, 
+  VStack, 
+  Menu, 
+  MenuButton,
+  MenuList,
+  MenuItem
+  } from '@chakra-ui/react'
+import { ChevronDownIcon } from '@chakra-ui/icons';
 import NextLink from 'next/link'
 
 export default function ChakraNavBar() {
-  return (
-    <Box
-      bg={useColorModeValue('black')}
-      color={useColorModeValue('white')}
-    >
-      <Container
-        as={Stack}
-        maxW={'9xl'}
-        py={5}
-        >
-        <Flex alignItems={'center'} justifyContent={'space-between'}>
 
-            <Text fontSize='4xl' as="a" fontFamily='sans-serif' href='/'>
-                Eric Ontiveros
-            </Text>
-            <HStack spacing={'100px'} >
-            <Box as="a" fontWeight="bold" href='/about' >
-                About
-            </Box>
-            <Box as="a" fontWeight="bold" href='/projects' w='300px'>
-                Projects
-            </Box>
-            <Link as={NextLink} href='https://www.linkedin.com/in/eontiveros' isExternal>
-              LinkedIn
-            </Link>
-            </HStack>
-        </Flex>
-      </Container>
-    </Box>
+  const [isSmallerThan768] = useMediaQuery("(max-width: 768px)");
+  return (
+    <Flex p="4" bg="black" color="white" alignItems="center" justifyContent="center" flexWrap="wrap" textAlign="center">
+      <VStack>
+        <Box mb={isSmallerThan768 ? 4 : 0}>
+          <Heading size="xl" fontFamily='sans-serif' as='a' href='/'>Eric Ontiveros</Heading>
+        </Box>
+        <Box mt={isSmallerThan768 ? 4 : 0}>
+          <Link mx="15" fontSize="xl" as="a" fontWeight="bold" href='/about'>
+            About
+          </Link>
+          <Link mx="20" fontSize="xl" as="a" fontWeight="bold" href='/projects'>
+            Projects
+          </Link>
+          <Menu>
+            <MenuButton mx="4" fontSize="xl">
+              Contact <ChevronDownIcon />
+            </MenuButton>
+            <MenuList>
+              <MenuItem 
+                color='black' 
+                as={NextLink} 
+                href='https://www.linkedin.com/in/eontiveros' 
+                isExternal
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                LinkedIn
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        </Box>
+      </VStack>
+    </Flex>
   )
 }
 
--- app/page.js
+++ app/page.js
@@ -9,6 +9,8 @@ import {
   Stack,
   Image,
   Flex,
+  HStack,
+  useMediaQuery,
   Spacer,
   
 } from '@chakra-ui/react'
@@ -16,43 +18,73 @@ import selfImage from './api/data/images/self-image3.png'
 const IMAGE = '/_next/static/media/self-image3.f98e0bfe.png'
 
 export default function ProductSimple() {
+  const [isSmallerThan768] = useMediaQuery("(max-width: 768px)");
   return (
-    <Center 
-      width='100%' 
-      height='100%'
-      textAlign={'center'}
-      // border='1px solid white'
-      p="40px"
+    // <Center 
+    //   width='100%' 
+    //   height='100%'
+    //   textAlign={'center'}
+    //   // border='1px solid white'
+    //   p="40px"
+    // >
+    //   <Flex
+    //     minWidth='max-content' 
+    //     alignItems='center'
+    //     gap='20'
+    //   >
+    //     <Box>
+    //     <Heading 
+    //       color={'white'} 
+    //       fontFamily='sans-serif'  
+    //       fontSize='7vw'
+    //     >
+    //       Hi, I'm Eric
+    //     </Heading>
+    //     <Text 
+    //       color='white'
+    //       fontSize="2vw"
+    //     >
+    //       Web designer, developer and tech enthusiast!
+    //     </Text>
+    //     </Box>
+    //     <Spacer />
+    //       <Box>
+    //           <Image
+    //             src={IMAGE}
+    //             alt="#"
+    //             borderRadius="15px 90px"
+    //           />
+    //         </Box>
+    // </Flex>
+    // </Center>
+    <Flex 
+      alignItems="center" 
+      justifyContent="center" 
+      p="14"
     >
-      <Flex
-        minWidth='max-content' 
-        alignItems='center'
-        gap='20'
-      >
-        <Box>
-        <Heading 
-          color={'white'} 
-          fontFamily='sans-serif'  
-          fontSize='7vw'
-        >
-          Hi, I'm Eric
-        </Heading>
-        <Text 
-          color='white'
-          fontSize="2vw"
-        >
-          Web designer, developer and tech enthusiast!
-        </Text>
+      <HStack>
+        <Box color='white' mb={isSmallerThan768 ? 4 : 0} mx='20'>
+          <Heading 
+            fontSize='7vw'
+            fontFamily='sans-serif' 
+          >
+            Hi, I'm Eric
+          </Heading>
+          <Text 
+            fontSize="2vw"
+          >
+            I enjoy web designing, developing and most things tech!
+          </Text>
         </Box>
-        <Spacer />
-          <Box>
-              <Image
-                src={IMAGE}
-                alt="#"
-                borderRadius="15px 90px"
-              />
-            </Box>
+        <Box mb={isSmallerThan768 ? 4 : 0} >
+          <Image
+            src={IMAGE}
+            alt="#"
+            borderRadius="15px 90px"
+            
+          />
+        </Box>
+      </HStack>
     </Flex>
-    </Center>
   )
 }
